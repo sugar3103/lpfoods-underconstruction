ce6bd1f47761d00a18db9e8d7f6143b7ac7eb499
sugar3103 <duong16628@gmail.com>
2019-11-26 10:19:05 +0700

    first time add

---
+++ build/precache-manifest.a3d7d6f69dadd5b832cbfd4931dab0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f290fe7a39a694a68859f38fa140ebc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "928c5767d4bbbc7b89cf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f2d4aa671c9044c02cb7",
+    "url": "/static/js/2.fff996c0.chunk.js"
+  },
+  {
+    "revision": "928c5767d4bbbc7b89cf",
+    "url": "/static/js/main.115d06c4.chunk.js"
+  },
+  {
+    "revision": "cc88c50a240aaebd443a",
+    "url": "/static/js/runtime-main.05299306.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
